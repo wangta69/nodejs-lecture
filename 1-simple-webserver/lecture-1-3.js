49afa9aa33f586e5705f2169fa304ec6b10efd14
younghyeong ryu <wangta69@naver.com>
2020-11-19 19:04:25 +0900

    Update lecture-1-3.js

--- 1-simple-webserver/lecture-1-3.js
+++ 1-simple-webserver/lecture-1-3.js
@@ -1,17 +1,22 @@
 const http = require('http');
 const fs = require('fs'); // load fs
-const app = http.createServer((request, response)=>{
+const path = require('path');
+const app = http.createServer((request, response) => {
+    // const u_name = req.param('name');
+    // const u_age = req.param('age');
     let url = request.url;
     if (request.url === '/') {
         url = '/lecture-1-3.html';
     }
-    if (request.url == '/my-home') {
+    if (request.url === '/my-home') {
         url = '/lecture-1-3-my-home.html';
     }
     if (request.url === '/favicon.ico') {
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    // response.end(fs.readFileSync(__dirname + url));
+    response.end(fs.readFileSync(path.join(__dirname, url)));
+//    res.sendFile(path.join(__dirname, '/public/chat.html'));
 });
 app.listen(3000);
